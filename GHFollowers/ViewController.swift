a94a9cf4113c24d38bac16244ba96c52101b8383
fredlc <fredlefevre@outlook.com>
2021-01-26 19:18:54 -0500

    Initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Fred Lefevre on 2021-01-26.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
