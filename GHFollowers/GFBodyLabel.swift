86961700accd70acfc301f64007e56ac078d9068
fredlc <fredlefevre@outlook.com>
2021-03-05 19:39:22 -0500

    Created custom alert view

---
+++ GHFollowers/GFBodyLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by Fred Lefevre on 2021-03-05.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -38,7 +38,10 @@ class SearchVC: UIViewController {
     }
     
     @objc func pushFollowersListVC() {
-        guard isUsernameEntered else { return }
+        guard isUsernameEntered else {
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😀.", buttonTitle: "Ok")
+            return
+        }
         let followersListVC = FollowersListVC()
         followersListVC.username = usernameTextField.text
         followersListVC.title = usernameTextField.text
