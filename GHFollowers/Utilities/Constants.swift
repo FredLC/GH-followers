e68af6f8dad504505c4169285a12251345266f2c
fredlc <fredlefevre@outlook.com>
2021-03-24 15:42:17 -0400

    Implemented UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Fred Lefevre on 2021-03-24.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
