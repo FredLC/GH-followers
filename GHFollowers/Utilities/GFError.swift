149372856b5a45cb23f13773a575ebaa6536788b
fredlc <fredlefevre@outlook.com>
2021-03-25 13:56:22 -0400

    Implemented PersistenceManager to save, add and remove favorites

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -13,4 +13,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request, please check your internet connection."
     case invalidResponse = "Invalid response from the server, please try again."
     case invalidData = "The data received from the server was invalid, please try again."
+    case unableToFavorite = "There was an error favoriting this user, please try again."
+    case alreadyInFavorites = "You've already favorited this user, you must REALLY like them!"
 }
